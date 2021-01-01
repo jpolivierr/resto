dfa37fe9f385501f6282e10eda651029d447e0cf
Frederic Olivier <jpolivierr@gmail.com>
2020-12-31 22:38:07 -0500

    updated server features

--- server.js
+++ server.js
@@ -6,27 +6,6 @@ const cors = require("cors")
 const Database = require("./Database/database")
 const path = require("path")
 
-// ** MIDDLEWARE ** //
-// const whitelist = [
-//   "http://localhost:5000",
-//   "https://https://murmuring-mesa-28934.herokuapp.com",
-// ]
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // console.log(origin)
-//     console.log("** Origin of request " + origin)
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       console.log("Origin acceptable")
-//       callback(null, true)
-//     } else {
-//       console.log("Origin rejected")
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   },
-// }
-
-// app.use(cors())
-
 Database()
 app.use(cors())
 //Init Middleware
@@ -40,13 +19,10 @@ app.use("/getRestaurant", require("./routes/getRestaurants"))
 
 app.use(express.static(path.join(__dirname, "client/build")))
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  // Handle React routing, return all requests to React app
+
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"))
   })
-}
 
 app.listen(PORT, () => {
   console.log(`-----------------------server is running on PORT:${PORT}`)
